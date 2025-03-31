3c8913736f925e18ce66a6729f6f54e49d371d20
YanikRijgersberg <9024941@student.zadkine.nl>
2025-03-31 21:07:08 +0100

    f

---
+++ app.js
@@ -0,0 +1,74 @@
+// let broek1 = Image/"broekv1-cocept.avif"
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let items1 = ["Appel", "Banaan", "Kers", "Druif", "Mango", "Peer"];
+//     let items2 = ["Appel", "Banaan", "Kers", "Druif", "Mango", "Peer"];
+//     let items3 = ["Appel", "Banaan", "Kers", "Druif", "Mango", "Peer"];
+//     let button = document.getElementById("vkleding");
+//     let result = document.createElement("p");
+
+  
+//     if (button) {
+//       button.addEventListener("click", function () {
+//         let randomIndex1 = Math.floor(Math.random() * items1.length);
+//         result.textContent = items1[randomIndex1];
+//         let randomIndex2 = Math.floor(Math.random() * items1.length);
+//         result.textContent = items2[randomIndex2];
+//         let randomIndex3 = Math.floor(Math.random() * items1.length);
+//         result.textContent = items3[randomIndex3];
+//       });
+//       document.body.appendChild(result);
+//     } 
+//     });
+document.addEventListener("DOMContentLoaded", function () {
+    let items = [
+      "./img/tspmo.avif",
+  
+    ];  //   "./img/banaan.png",
+    //   "kers.png",
+    //   "druif.png",
+    //   "mango.png",
+    //   "peer.png"
+    let button = document.getElementById("vkleding");
+    const result = document.createElement("img");
+    result.style.maxWidth = "200px";
+    result.style.marginTop = "10px";
+    result.style.display = "block";
+  
+    if (button) {
+      button.addEventListener("click", function () {
+        let randomIndex = Math.floor(Math.random() * items.length);
+        result.src = items[randomIndex];
+        result.alt = "Gekozen afbeelding";
+      });
+      document.body.appendChild(result);
+    } 
+    });
+  document.addEventListener("DOMContentLoaded", function () {
+    let items = [
+      "./img/broekv1-cocept.avif",
+      // "./img/banaan.png",
+    //   "kers.png",
+    //   "druif.png",
+    //   "mango.png",
+    //   "peer.png"
+    ];
+    let button = document.getElementById("vkleding");
+    const result = document.createElement("img");
+    result.style.maxWidth = "200px";
+    result.style.marginTop = "10px";
+    result.style.display = "block";
+  
+    if (button) {
+      button.addEventListener("click", function () {
+        let randomIndex = Math.floor(Math.random() * items.length);
+        result.src = items[randomIndex];
+        result.alt = "Gekozen afbeelding";
+      });
+      document.body.appendChild(result);
+    } 
+  });
