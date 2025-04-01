cc1e7df64d50be3371507611bf25c42b0c58f79b
YanikRijgersberg <9024941@student.zadkine.nl>
2025-04-01 09:19:41 +0100

    .

--- met-ts.js
+++ met-ts.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     let items = [
-     "./img/test/tspmo.avif",
-     "./img/test/tspmo2.avif",    
+     "./img/tshirt/tspmo.avif",
+     "./img/tshirt/tspmo2.avif",    
     ];  
     let button = document.getElementById("vkleding");
     const result = document.createElement("img");
@@ -18,6 +18,28 @@ document.addEventListener("DOMContentLoaded", function () {
       document.body.appendChild(result);
     } 
     });
+    //////////////////////////////////////////////////////////////////////////////
+    document.addEventListener("DOMContentLoaded", function () {
+        let items = [
+          "./img/riemen/black riem.png",
+        ];
+        let button = document.getElementById("vkleding");
+        const result = document.createElement("img");
+        result.style.maxWidth = "100px";
+        result.style.marginTop = "10px";
+        result.style.marginLeft = "50px";
+        result.style.display = "block";
+        result.style.alignContent = "center";
+        if (button) {
+          button.addEventListener("click", function () {
+            let randomIndex = Math.floor(Math.random() * items.length);
+            result.src = items[randomIndex];
+            result.alt = "Gekozen afbeelding";
+          });
+          document.body.appendChild(result);
+        } 
+      });
+    //////////////////////////////////////////////////////////////////////////////
   document.addEventListener("DOMContentLoaded", function () {
     let items = [
       "./img/broeken/edhadybroekg.png",
@@ -41,3 +63,25 @@ document.addEventListener("DOMContentLoaded", function () {
       document.body.appendChild(result);
     } 
   });
+  ///////////////////////////////////////////////////////////////////////////////
+  document.addEventListener("DOMContentLoaded", function () {
+    let items = [
+     "./img/schoenen/NIKE GREEN.png",
+     "./img/schoenen/fake campus.png",
+     "./img/schoenen/tims.png",
+    ];  
+    let button = document.getElementById("vkleding");
+    const result = document.createElement("img");
+    result.style.maxWidth = "150px";
+    result.style.marginTop = "10px";
+    result.style.display = "block";
+  
+    if (button) {
+      button.addEventListener("click", function () {
+        let randomIndex = Math.floor(Math.random() * items.length);
+        result.src = items[randomIndex];
+        result.alt = "afbeelding";
+      });
+      document.body.appendChild(result);
+    } 
+    });
